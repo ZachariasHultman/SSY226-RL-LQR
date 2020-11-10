cc19b27995cbf73863f351edfca293328646ccfd
adamkhalif <46897176+adamkhalif@users.noreply.github.com>
2020-11-10 13:26:04 +0100

    test

--- dynamic_system_simulation.py
+++ dynamic_system_simulation.py
@@ -18,16 +18,15 @@ def cart_pendulum_sim(t , x, L=1., m=1., M = 1., g=9.81, F=0, f=0):
 
     x_2_dot_denomi = (M + (1 - np.cos(x3) * np.cos(x3)) * m)
 
-    x_4_dot_nomi = (M+m)*(g*np.sin(x3)-f*x4) - \
-        (L*m*x4*x4*np.sin(x3)+F) * \
-        np.cos(x3)
+    x_4_dot_nomi = ((M+m)*(g*np.sin(x3)-f*x4) -
+                    (L*m*x4*x4*np.sin(x3)+F) * np.cos(x3))
 
     x_4_dot_denomi = L*x_2_dot_denomi
 
     x_1_dot = x2
     x_2_dot = x_2_dot_nomi/x_2_dot_denomi
     x_3_dot = x4
-    x_4_dot = x_4_dot_nomi/x_4_dot_denomi
+    x_4_dot = x_4_dot_nomi/x_4_dot_denomi ###
 
     return [x_1_dot, x_2_dot, x_3_dot, x_4_dot]
 
