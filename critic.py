d9fb88c17194c2c2a9e8abbe68b96d4d4bcefa2b
adamkhalif <46897176+adamkhalif@users.noreply.github.com>
2020-11-17 19:17:09 +0100

    push to zacharias

--- critic.py
+++ critic.py
@@ -4,89 +4,69 @@ from tools import kronecker
 # equation 20 is the following function
 
 
-class critic():
-    def __init__(self, n, m, alpha):
-        s=(int(1/2*((n+m)*(n+m+1))),1)
-        self.W = np.ones(s)
-        self.error = np.Inf
-        self.alpha = alpha
-        self.n = n
-        self.m = m
+def approx_update(x, x_prev, u, u_prev, W_c_hat, W_c_tilde, alpha_c, M, R, T, n ,m):
 
-    def approx_update(self, x, x_prev, u, u_prev, M, R, T):
+    U = np.concatenate((x.T, u.T)).T
+    U_prev = np.concatenate((x_prev.T, u_prev.T)).T
 
-        U = np.concatenate((x.T, u.T)).T
-        U_prev = np.concatenate((x_prev.T, u_prev.T)).T
-        print(U)
-        tol = 0.01  # Tolerance
-        error = tol+1  # Init error larger than tol
-        W_old=self.W
-        sigma = self.sigma_fun(u, u_prev)
-        # while error > tol:
-            
+    sigma = sigma_fun(U, U_prev, n, m)
 
-        # Compute new Weights
-        # See equation below (e=...) to understand the need of the integral term.
-        # The integral term is calculated by assumptions that self-defined matrices M and R are diagonal
-        # and assumption that the integration is discrete with time step T and only two points of evaluation
-
-        if u.shape[0]<2:
-            int_term = 0.5 * T * ((np.matmul(np.matmul(x.T, M), x) + u*R*u) +
-                                    np.matmul(np.matmul(x_prev.T, M), x_prev) + u_prev*R*u_prev)
-        else:
-            int_term = 0.5 * T * (np.matmul(np.matmul(x.T, M), x) + np.matmul(np.matmul(u.T, R), u) +
-                                    np.matmul(np.matmul(x_prev.T, M), x_prev) + np.matmul(np.matmul(u_prev.T, R), u_prev))
-
-        # Using integral RL gives error of (Bellman) value function as (eq.17 to eq.18)
-        e = np.matmul(self.W.T, kronecker(U, U,self.n,self.m)) + int_term - np.matmul(self.W.T, kronecker(U_prev, U_prev,self.n,self.m))
-
-        print('eureka')
-
-        # Update of the critic approximation weights (Equation 20)
-
-        self.W = -self.alpha * sigma / ((1 + np.matmul(sigma.T, sigma))**2) * e.T
-
-        # Calculates the error as 2-norm of the difference between the new and old W-matrix.
-        error_weights = W_old - self.W
-
-        error = -self.alpha * (np.matmul(sigma, sigma.T) / ((1 + np.matmul(sigma.T, sigma))**2))* error_weights
-        error_diff = np.linalg.norm(error - self.error, ord=2)
-        self.error = error
-
-        # Save old weights
-        W_old = self.W
+    # while error > tol:
 
 
-    def Q_uu(self):
-        n = self.n
-        m = self.m
-        # Extract Q_uu from Wc on vector form from
-        q_vec = self.W[n * (n + 1) / 2 + 1 + n * m:(n + m) * (n + m + 1) / 2]
-        # Reshape to matrix form
-        q_uu = q_vec.reshape((m, m))
-        return q_uu
+    # Compute new Weights
+    # See equation below (e=...) to understand the need of the integral term.
+    # The integral term is calculated by assumptions that self-defined matrices M and R are diagonal
+    # and assumption that the integration is discrete with time step T and only two points of evaluation
 
-    def Q_ux(self):
-        n = self.n
-        m = self.m
-        # Extract Q_xu from Wc on vector form from
-        q_vec = self.W[n * (n + 1) / 2 + 1:n * (n + 1) / 2 + n * m]
-        # Reshape to matrix form
-        q_xu = q_vec.reshape((self.n, self.m))
-        # Transpose to get Q_ux
-        q_ux = q_xu.T
-        return q_ux
+    if u.shape[0]<2:
+        int_term = 0.5 * T * ((np.matmul(np.matmul(x.T, M), x) + u*R*u) +
+                                np.matmul(np.matmul(x_prev.T, M), x_prev) + u_prev*R*u_prev)
+    else:
+        int_term = 0.5 * T * (np.matmul(np.matmul(x.T, M), x) + np.matmul(np.matmul(u.T, R), u) +
+                                np.matmul(np.matmul(x_prev.T, M), x_prev) + np.matmul(np.matmul(u_prev.T, R), u_prev))
 
-    def sigma_fun(self, U_curr, U_prev):
-        """
-        Help function to calculate sigma in critic weights equation
-        Parameters: U_curr, which is [states; control signal] ([x;u]) concatinated at the current time step. array_like. Size NxM
-                    U_prev, which is [states; control signal] ([x;u]) concatinated at the previous time step. array_like Size NxM
+    # Using integral RL gives error of (Bellman) value function as (eq.17 to eq.18)
+    e = np.matmul(W_c_hat.T, kronecker(U, U,n,m)) + int_term - np.matmul(W_c_hat.T, kronecker(U_prev, U_prev,n,m))
 
-        Out: sigma, array like. Size N^2xM^2
-        """
 
-        sigma_pt1 = np.kron(U_curr, U_curr)
-        sigma_pt2 = np.kron(U_curr, U_curr)
-        sigma = np.kron(sigma_pt1, sigma_pt2)
-        return sigma
+    # Update of the critic approximation weights (Equation 20)
+
+    W_c_hat_dot = -alpha_c * sigma / ((1 + np.matmul(sigma.T, sigma))**2) * e.T
+
+    W_c_tilde_dot = -alpha_c * (np.matmul(sigma, sigma.T) / ((1 + np.matmul(sigma.T, sigma))**2))* W_c_tilde
+
+    Q_xu_tilde = Q_xu(n, m, W_c_hat)
+
+    return W_c_hat_dot, W_c_tilde_dot, Q_xu_tilde
+
+def Q_uu(n,m,W_hat):
+
+    # Extract Q_uu from Wc on vector form from
+    q_vec = W_hat[int(n * (n + 1) / 2 + 1 + n * m):int((n + m) * (n + m + 1) / 2)]
+    # Reshape to matrix form
+    q_uu = q_vec.reshape((m, m))
+    return q_uu
+
+def Q_xu(n,m,W_hat):
+
+    # Extract Q_xu from Wc on vector form from
+    q_vec = W_hat[int(n * (n + 1) / 2 + 1):int(n * (n + 1) / 2 + n * m)]
+    # Reshape to matrix form
+    q_xu = q_vec.reshape((n, m))
+    return q_xu
+
+def sigma_fun(U_curr, U_prev, n, m):
+    """
+    Help function to calculate sigma in critic weights equation
+    Parameters: U_curr, which is [states; control signal] ([x;u]) concatinated at the current time step. array_like. Size NxM
+                U_prev, which is [states; control signal] ([x;u]) concatinated at the previous time step. array_like Size NxM
+
+    Out: sigma, array like. Size N^2xM^2
+    """
+
+    sigma_pt1 = kronecker(U_curr, U_curr, n, m)
+    sigma_pt2 = kronecker(U_prev, U_prev, n, m)
+
+    sigma = sigma_pt1 - sigma_pt2
+    return sigma
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 import control as ctrl
 from dynamic_system_simulation import cart_pendulum_sim
 from dynamic_system_simulation import cart_pendulum_sim_lqr
+from dynamic_system_simulation import cart_pendulum_sim_lqr2
+from dynamic_system_simulation import func
 from tools import cart_pendulum_lin_lqr_gain
-from actor import actor
-from critic import critic
 
-M = 0.5  # cart mass
+
+M_p = 0.5  # cart mass
 m_p = 0.2  # pendulum mass
 g = 9.81  # gravity
 L = 1  # pendulum length
@@ -29,49 +30,54 @@ Q = np.array([[ 1,         0,                 0,                  0],
 
 R = 0.01
 
-K = cart_pendulum_lin_lqr_gain(L, m_p, M, g, f, b, Q, R)  # LQR gain for linearized system
+K = cart_pendulum_lin_lqr_gain(L, m_p, M_p, g, f, b, Q, R)  # LQR gain for linearized system
 
-args = (L, m_p, M, g, F, f, b)  # arguments for non-linear system
-args_lqr = (K, L, m_p, M, g, f, b)  # arguments for controlled linear system
+args = (L, m_p, M_p, g, F, f, b)  # arguments for non-linear system
+args_lqr = (K, L, m_p, M_p, g, f, b)  # arguments for controlled linear system
 
 vals = integrate.solve_ivp(cart_pendulum_sim, t_span, x_init, args=args, t_eval=t_eval)
 vals_lqr = integrate.solve_ivp(cart_pendulum_sim_lqr, t_span, x_init_lqr, args=args_lqr, t_eval=t_eval)
 # x_vals = integrate.odeint(cart_pendulum_sim, x_init, t_span)
 
 # Simulation with Actor-Critic
+n = 4
+m = 1
+s = int(1 / 2 * ((n + m) * (n + m + 1)))
 T = 0.05  # delta t [s]
 t_eval = np.linspace(t_span[0], t_span[1], 200)
-x_ac = np.ndarray(shape=(4, 1))
-x_ac[:, 0] = x_init_lqr
+x_ac = np.zeros(shape=(n+n+s+s, 1))
+x_ac[:n, 0] = x_init_lqr
+
 t_ac = np.ndarray(shape=(1, 0))
-actor = actor(n=4, m=1, alpha=2)
-critic = critic(n=4, m=1, alpha=50)
-u_prev = np.zeros(1)
-x_prev = x_ac[:, 0]
+
+alpha_c = 50
+alpha_a = 2
+s = int(1 / 2 * ((n + m) * (n + m + 1)))
+K = np.ones((n,m))
+M = np.identity(n)
+R = np.identity(m)
 for t in t_eval:
-    u = -np.matmul(K, x_ac[:, -1])
+
+    u = -np.matmul(K.T, x_ac[:n, -1])
     
     u = np.atleast_1d(u)
 
-    x = x_ac[:, -1]
-    critic.approx_update(x, x_prev, u, u_prev, Q, R, T)
-    q_vec_tmp = self.W[critic.n * (critic.n + 1) / 2 + 1:critic.n * (critic.n + 1) / 2 + critic.n * critic.m]
-    Q_xu_tilde= q_vec_tmp.reshape((critic.n, critic.m))
-    actor.approx_update(critic.Q_uu(), critic.Q_ux(), x,Q_xu_tilde)
-    K = actor.W
+    x = x_ac[:n, -1]
+
     u_prev = u
     x_prev = x
-    args_ac = (K, L, m, M, g, f, b)
+    args_ac = (cart_pendulum_sim_lqr2, L, m_p, M_p, g, F, f, b, n , m, x_prev, u_prev, alpha_c, alpha_a, M, R, T)
 
     t_span_ac = [t, t+T]
 
-    vals_ac = integrate.solve_ivp(cart_pendulum_sim_lqr, t_span_ac, x_ac[:, -1], args=args_ac)
+    vals_ac = integrate.solve_ivp(func, t_span_ac, x_ac[:, -1], args=args_ac)
 
+    K = vals_ac.y[n:s]
     # Save time and state values
-    x_ac = np.concatenate((x_ac, vals_ac.y), axis=1)
+    x_ac = np.concatenate((x_ac, vals_ac.y[:n]), axis=1)
     t_ac = np.concatenate((t_ac, vals_ac.t), axis=0)
 
-
+    print("eureka")
 
 
 # Plotting non-linear system
